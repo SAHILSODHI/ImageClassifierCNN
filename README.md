a54fe6ec56888b11ad0ab9e798a58a3dd0b6cfa3
sahil sodhi <sahilsodhi@sahils-MacBook-Pro.local>
2019-02-24 02:33:13 -0500

    Final Commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ImageClassifierCNN
 Animal's Image Classification Using Convolutional Neural Networks
+
+Dataset: https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/14_page_p8s40_file_1.zip
---
+++ cnn.py
@@ -0,0 +1,73 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+# we are using 2d because we are not having a video which is 3d because of time.
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Add Convolution Layer
+#Convolution2D(no_of_feature_detector, number of rows of feature detector, number of columns of feature detector,
+# expected format of input images(corrected in image preprocessing), 
+# input_shape = (dimeansions of image, number of channels(rbf)), activation is rectifier to maintain nonlinearity).
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening... put all pooled feature maps to a vector. Input to ANN.
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,   #pixel value between 1 to 255 will be rescaled between 0 and 1.
+                                   shear_range = 0.2,  # random transformations
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         samples_per_epoch = 8000,
+                         nb_epoch = 25,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)
